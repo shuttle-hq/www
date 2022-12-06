57b2df67ba3e9f7346596ba30f7a1133a529f7b2
Senofy <hello@digitalera.dev>
2022-12-05 20:42:26 -0500

    Setup tailwind and prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
 export default function Home() {
-  return (
-    <><div>Test</div></>
-  )
+	return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
 }
