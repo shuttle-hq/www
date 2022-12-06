6717f15624846579c1aad8000cbe88e4dfa0b256
Senofy <hello@digitalera.dev>
2022-12-05 20:50:31 -0500

    Setup _app.tsx with googleanalytics + seo + cookie-consent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,114 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
+import CookieConsent from 'react-cookie-consent'
+import Head from 'next/head'
+import Script from 'next/script'
+
+import {
+	APP_NAME,
+	SITE_TITLE,
+	SITE_DESCRIPTION,
+	SITE_URL,
+	TWITTER_HANDLE,
+	GA_MEASUREMENT_ID,
+} from '../lib/constants'
+import { gtagConsent, gtagRevokeConsent, setupGoogleAnalytics } from '../lib/gtag'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+const transitionClass = 'transition hover:brightness-125'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const router = useRouter()
+	const { user } = pageProps
+	useEffect(() => setupGoogleAnalytics(router, user))
+
+	return (
+		<>
+			<Head>
+				<title>{SITE_TITLE}</title>
+			</Head>
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
+			<Script
+				strategy='afterInteractive'
+				src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+			/>
+			{/* Disable ad storage always. Disable analytics storage by default for EU and EEA countries,
+          allowing users to opt-in via the cookie consent banner. Analytics storage is enabled by default
+          for remaining countries, but they may opt-out via cookie consent banner.*/}
+			<Script
+				id='google-analytics'
+				strategy='afterInteractive'
+				dangerouslySetInnerHTML={{
+					__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('consent', 'default', {
+              'analytics_storage': 'denied',
+              'region': ['BE', 'BG', 'CZ', 'DK', 'DE', 'EE', 'IE', 'GR', 'ES', 'FR', 'HR', 'IT', 'CY', 'LV', 'LT', 'LU', 'HU', 'MT', 'NL', 'AT', 'PL', 'PT', 'RO', 'SI', 'SK', 'FI', 'SE', 'NO', 'CH', 'IS', 'LI', 'UK'],
+            });
+            gtag('consent', 'default', {
+              'ad_storage': 'denied',
+            });
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+				}}
+			/>
+			<DefaultSeo
+				title={APP_NAME}
+				description={SITE_DESCRIPTION}
+				openGraph={{
+					type: 'website',
+					url: SITE_URL,
+					site_name: APP_NAME,
+				}}
+				twitter={{
+					handle: TWITTER_HANDLE,
+					site: TWITTER_HANDLE,
+					cardType: 'summary_large_image',
+				}}
+			/>
+			<div className='dark:bg-dark-700 dark:text-dark-200 min-h-screen bg-slate-100 text-slate-800'>
+				<Component {...pageProps} />
+
+				<CookieConsent
+					style={{
+						justifyContent: 'center',
+					}}
+					enableDeclineButton={true}
+					declineButtonText='Decline'
+					// only a limited subset of tailwind classes work
+					declineButtonClasses={transitionClass}
+					declineButtonStyle={{
+						borderRadius: '4px',
+						fontWeight: '500',
+						color: 'rgb(15 23 42)',
+						padding: '4px 16px',
+						backgroundColor: '#7777dd',
+						minWidth: '87px',
+						opacity: '0.7',
+					}}
+					buttonText='Accept'
+					// only a limited subset of tailwind classes work
+					buttonClasses={transitionClass}
+					buttonStyle={{
+						borderRadius: '4px',
+						fontWeight: '500',
+						color: 'rgb(15 23 42)',
+						padding: '4px 16px',
+						backgroundColor: '#f25100',
+						minWidth: '87px',
+					}}
+					onDecline={() => gtagRevokeConsent()}
+					onAccept={() => gtagConsent()}
+				>
+					We use cookies to enhance the user experience and measure engagement.
+				</CookieConsent>
+			</div>
+		</>
+	)
 }
