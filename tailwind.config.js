db108a49d474053156a64dbffad5212d91e2d762
Senofy <hello@digitalera.dev>
2022-12-06 21:06:44 -0500

    Updated tailwind file with new settings for sizes and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,18 @@ module.exports = {
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				gradual: 'sans-serif',
+			},
 			borderRadius: {
 				button: '0.875rem',
+			},
+			fontSize: {
+				4.5: '2.5rem',
+				6.5: '4rem',
+			},
+			screens: {
+				desktop: '1440px',
 			},
 		},
 	},
