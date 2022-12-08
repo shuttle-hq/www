f23fc8e145eabbe52e0090408057b78f6a695938
Senofy <hello@digitalera.dev>
2022-12-07 23:08:32 -0500

    Implemented Testimonials

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import {
 	Hero,
 	HowItWorks,
 	LogosReferences,
+	Testimonials,
 } from 'components/sections'
 
 export default function Home() {
@@ -18,6 +19,7 @@ export default function Home() {
 			<CommunitySupportedNumbers />
 			<FeaturedStarters />
 			<HowItWorks />
+			<Testimonials />
 		</>
 	)
 }
