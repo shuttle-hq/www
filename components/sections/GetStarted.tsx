da128e73f8289176aaa45eaab018eebd167d8f79
Senofy <hello@digitalera.dev>
2022-12-09 19:24:07 -0500

    Fix GetStarted Steps Colors for Light Mode

--- components/sections/GetStarted.tsx
+++ components/sections/GetStarted.tsx
@@ -107,12 +107,12 @@ interface GetStartedSlideProps {
 
 const GetStartedSlide: FC<GetStartedSlideProps> = ({ number, title, text, children }) => {
 	return (
-		<SplideSlide className='max-w-[85%] p-[1px] lg:max-w-full lg:pb-4 lg:last:pb-0'>
+		<SplideSlide className='max-w-[85%] p-[1px] lg:max-w-full lg:pb-4 lg:last:pb-1'>
 			<div
 				className={clsx(
-					'border-gradient group relative h-full cursor-pointer cursor-pointer rounded-2xl rounded-2xl p-6 p-6 outline outline-1 outline-black/10 transition after:rounded-2xl hover:outline-none dark:bg-black dark:outline-[#191919]',
-					number !== '1' && // If it isn't the first slide, hide the outline on desktop
-						'lg:outline-none'
+					'border-gradient group relative h-full cursor-pointer cursor-pointer rounded-2xl rounded-2xl bg-white p-6 p-6 outline outline-1 outline-black/10 transition after:rounded-2xl hover:outline-none dark:bg-black hover:lg:outline-none',
+					number === '1' && // If it isn't the first slide, hide the outline on desktop
+						'dark:outline-[#191919]'
 				)}
 			>
 				<h3 className='font-gradual text-2xl font-bold dark:text-[#C2C2C2]'>
