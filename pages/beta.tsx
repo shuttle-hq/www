ad12e780cfd437d160c7d9ab44471c78ac0f69b5
Senofy <hello@digitalera.dev>
2022-12-12 19:19:15 -0500

    Added form submit on beta

--- pages/beta.tsx
+++ pages/beta.tsx
@@ -1,9 +1,17 @@
 import { Button } from 'components/elements'
 import { Page } from 'components/templates'
 import Image from 'next/image'
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 
 export default function Beta() {
+	const [formSubmitted, setFormSubmitted] = useState(false)
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		setFormSubmitted(true)
+	}
+
 	return (
 		<div className='group relative flex-grow overflow-hidden'>
 			<span className='pointer-events-none'>
@@ -43,20 +51,48 @@ export default function Beta() {
 						backend framework with the fastest build, test and deploy times ever.
 					</p>
 				</header>
-				<form className='mt-12 w-full max-w-xl flex-col rounded-2xl bg-black/20 p-5 backdrop-blur-[15px] sm:p-8'>
-					<h3 className='text-xl'>Your email</h3>
-					<div className='input-border-gradient relative mt-2 rounded-xl bg-black focus-within:bg-black hover:bg-black'>
-						<input
-							className='w-full rounded-xl bg-transparent py-3 px-4 text-xl text-white outline outline-1 outline-[#7A7A7A] placeholder:text-[#C2C2C2]/20 hover:outline-transparent focus:outline-transparent'
-							placeholder='picard@enterprise.com'
-							type='email'
-							required
-						/>
+				{!formSubmitted ? (
+					<form
+						className='mt-12 w-full max-w-xl flex-col rounded-2xl bg-black/20 p-5 backdrop-blur-[15px] sm:p-8'
+						onSubmit={handleSubmit}
+					>
+						<h3 className='text-xl'>Your email</h3>
+						<div className='input-border-gradient relative mt-2 rounded-xl bg-black focus-within:bg-black hover:bg-black'>
+							<input
+								className='w-full rounded-xl bg-transparent py-3 px-4 text-xl text-white outline outline-1 outline-[#7A7A7A] placeholder:text-[#C2C2C2]/20 hover:outline-transparent focus:outline-transparent'
+								placeholder='picard@enterprise.com'
+								type='email'
+								required
+							/>
+						</div>
+						<Button type='submit' variant='primary' className='mt-5 bg-[#D8D8D8] px-6 py-3 text-lg'>
+							Sign up for beta
+						</Button>
+					</form>
+				) : (
+					<div className='mt-12 w-full max-w-xl flex-col rounded-2xl bg-black/20 p-5 backdrop-blur-[15px] sm:p-8'>
+						<h3 className='font-gradual text-[2rem] font-bold leading-none text-white'>
+							Thank you for signing up!
+						</h3>
+						<p className='mt-3'>
+							If you are looking for a way to support shuttle in the meantime, you can do so by:
+						</p>
+						<div className='mt-8 flex flex-col items-start'>
+							<a href='https://github.com/shuttle-hq/shuttle' target='_blank'>
+								💻 contributing to shuttle
+							</a>
+							<a href='https://github.com/shuttle-hq/shuttle' target='_blank'>
+								⭐️ starring the repository
+							</a>
+							<a href='https://discord.gg/shuttle' target='_blank'>
+								👾 joining our discord community
+							</a>
+							<a href='https://twitter.com/shuttle_dev' target='_blank'>
+								🐦 following us on twitter
+							</a>
+						</div>
 					</div>
-					<Button variant='primary' className='mt-5 bg-[#D8D8D8] px-6 py-3 text-lg'>
-						Sign up for beta
-					</Button>
-				</form>
+				)}
 			</div>
 		</div>
 	)
