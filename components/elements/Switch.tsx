9fd0fd70be80ed912f5b684ec888abeaeadae992
enofy <hello@digitalera.dev>
2022-12-21 19:25:58 -0800

    Updated switch background for light mode

--- components/elements/Switch.tsx
+++ components/elements/Switch.tsx
@@ -7,7 +7,7 @@ interface SwitchProps {
 
 const Switch: FC<SwitchProps> = ({ checked, setChecked }) => {
 	return (
-		<label className='border-gradient always-visible relative h-[32px] w-[77px] cursor-pointer rounded-3xl bg-black after:rounded-3xl'>
+		<label className='border-gradient always-visible relative h-[32px] w-[77px] cursor-pointer rounded-3xl bg-white after:rounded-3xl dark:bg-black'>
 			<input
 				type='checkbox'
 				className='peer hidden'
