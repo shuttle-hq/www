f252856f3eba923007044f8b56e6e43d69c6f542
Senofy <hello@digitalera.dev>
2023-02-15 21:53:25 -0500

    Move Job Posts to Content Folder

---
+++ content/jobs.ts
@@ -0,0 +1,18 @@
+import { PositionProps } from 'components/sections/JobOpenings'
+
+export const Jobs: PositionProps[] = [
+	{
+		name: 'Backend Engineer (Rust)',
+		link: 'https://www.workatastartup.com/jobs/57932',
+	},
+	{
+		name: 'Framework Engineer',
+		link: 'https://www.workatastartup.com/jobs/57934',
+	},
+	{
+		name: 'Technical Product Manager',
+		link: 'https://www.workatastartup.com/jobs/57935',
+	},
+]
+
+export default Jobs
