9634281a61238101846dcda4b5b999fb55f55826
Senofy <hello@digitalera.dev>
2023-02-19 23:16:49 -0500

    Made letter spacing tighter for cookie consent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,7 +41,7 @@ export default function App({ Component, pageProps }: AppProps) {
 				{getLayout(<Component {...pageProps} />)}
 				<CookieConsent
 					containerClasses='max-w-xl left-1/2 transform bottom-4 -translate-x-1/2 flex items-end flex-col bg-black/10 border border-white/10 backdrop-filter backdrop-blur-lg backdrop-saturate-150 rounded-2xl p-6'
-					contentClasses='text-base text-[#FFFFFF8F] !m-0 !flex-none'
+					contentClasses='text-base text-[#FFFFFF8F] !m-0 !flex-none tracking-tight self-start'
 					buttonWrapperClasses='!mt-3'
 					buttonClasses='!m-0 !py-3 !px-6 gap-2 whitespace-nowrap rounded-button font-bold transition-all duration-500 button-shadow dark:border-gradient dark:shadow-gradient bg-[#E9E9E9] text-black hover:bg-gradient-to-r hover:from-[#fc540c] hover:to-[#f5c57a] hover:text-white dark:bg-black dark:text-[#C2C2C2] dark:hover:bg-none'
 					declineButtonClasses='!p-0 !mr-6 !ml-0 !my-0 !bg-transparent text-[#7A7A7A] hover:text-[#FFFFFF] underline transition duration-500'
