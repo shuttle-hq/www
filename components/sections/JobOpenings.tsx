68fc8e0601fb06280b652944f97fafceae34409e
Damien <git@damien.sh>
2023-02-20 15:12:05 +0000

    misc: update the jobs board

--- components/sections/JobOpenings.tsx
+++ components/sections/JobOpenings.tsx
@@ -41,14 +41,11 @@ const JobOpenings = () => {
 					<div className='mt-8 grid gap-6 lg:mt-16 lg:grid-cols-2 lg:items-start lg:gap-8'>
 						<div className='space-y-6 text-xl text-[#FFFFFF8F] dark:text-[#C2C2C2] lg:space-y-8'>
 							<p>
-								Joining us, you&apos;ll become part of an exceptionally strong engineering team
+								Joining us, you&apos;ll become part of an exceptionally strong team
 								that has a big bias for action and a love for quick experimentation and hacking.
 							</p>
 							<p>
-								We have a flat hierarchy and build everything in the open - our main tools are our
-								GH and Discord, which are both completely public. We believe everyone should
-								contribute equally to the engineering decisions as well as our products&apos;
-								direction.
+								Let&apos;s build something people want, together!
 							</p>
 						</div>
 						<div className='flex flex-col divide-y divide-white/10 border-b border-white/10 text-xl text-[#C2C2C2]'>
