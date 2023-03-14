6f0c46ff2c17563b348bfe551b9eee270b390ca4
Guga Gongadze <gugongadze@gmail.com>
2023-03-14 19:18:50 +0400

    feat: missions bg image

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,21 @@
 import { AboutHero, BackedBy, JobOpenings, Mission, WorkingWithUs } from 'components/sections'
+import Image from 'next/image'
 
 export default function About() {
 	return (
-		<>
+		<main>
+			<Image
+				src='/images/sections/mission/mission.jpg'
+				alt='bg'
+				fill
+				style={{ objectFit: 'cover', zIndex: -1, top: '50%' }}
+				quality={100}
+			/>
 			<AboutHero />
 			<Mission />
 			<BackedBy />
 			<WorkingWithUs />
 			<JobOpenings />
-		</>
+		</main>
 	)
 }
