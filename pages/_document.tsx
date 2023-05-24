8a78c6ef4415a1597c9e87aa1a728d556bd9b867
Guga Gongadze <guga@shuttle.rs>
2023-05-24 17:00:03 +0400

    fix: reset initial scale for first page load (#69)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
 					href='https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible&display=swap'
 					rel='stylesheet'
 				/>
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 			</Head>
 			<body className='dark'>
 				<Main />
