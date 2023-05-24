22108992f07fe384bf97bee6ff8a8bec50519cee
Senofy <hello@digitalera.dev>
2023-05-23 21:29:56 -0400

    Setup Nav Background & Hero for Shuttle AI

--- components/sections/ShuttleAI/Hero.tsx
+++ components/sections/ShuttleAI/Hero.tsx
@@ -1,5 +1,38 @@
 const Hero = () => {
-	return <div></div>
+	return (
+		<div className='relative flex flex-col items-center justify-center px-4 pt-60'>
+			<h1 className='w-fit text-center font-gradual text-5xl font-bold text-black dark:text-[#C2C2C2] lg:text-6.5'>
+				Instant apps,
+				<br />
+				<span className=''>
+					from a <span className='text-gradient bg-clip-text text-transparent'>single prompt</span>
+				</span>
+			</h1>
+			<p className='mt-8 text-center text-xl text-[#7A7A7A]'>
+				Yes, we did it. From prompt to deployed app in under 5 minutes
+			</p>
+
+			<form
+				action='https://buttondown.email/api/emails/embed-subscribe/shuttle'
+				method='post'
+				target='popupwindow'
+				className='embeddable-buttondown-form mt-10 text-center'
+			>
+				<input
+					type='email'
+					name='email'
+					placeholder='Your email'
+					className='mr-[18px] h-[56px] w-full rounded-xl border border-slate-700 bg-transparent px-[12px] py-[15px] text-xl font-normal text-[#D8D8D8] placeholder:text-[#D8D8D8] sm:w-[412px]'
+				/>
+				<input type='hidden' value='1' name='embed' />
+				<input
+					type='submit'
+					value='Join the waitlist'
+					className='mt-4 h-[56px] w-full rounded-[14px] bg-[#D8D8D8] text-lg text-black sm:mt-0 sm:w-[154px]'
+				/>
+			</form>
+		</div>
+	)
 }
 
 export default Hero
--- pages/shuttle-ai.tsx
+++ pages/shuttle-ai.tsx
@@ -1,14 +1,37 @@
 import { Hero } from 'components/sections/ShuttleAI'
 import { Waitlist } from 'components/sections/Launchpad'
+import { Page } from 'components/templates'
+import { ReactNode } from 'react'
+import Image from 'next/image'
 
 export default function ShuttleAIPage() {
 	return (
-		<>
+		<div className='group relative flex-grow overflow-hidden'>
+			<Image
+				src='/images/pages/shuttle-ai/noise-group.png'
+				alt=''
+				width={1440}
+				height={1023}
+				className='absolute -top-[5.5rem] w-full mix-blend-overlay'
+			/>
+			<Image
+				src='/images/pages/shuttle-ai/stars.png'
+				alt=''
+				width={1427}
+				height={321}
+				className='absolute -top-[5.5rem] w-full'
+			/>
 			<Hero />
 			<Waitlist
 				cta='The fastest way to go from an idea to production.'
 				description='Sign up for the waitlist!'
 			/>
-		</>
+		</div>
 	)
 }
+
+ShuttleAIPage.getLayout = (children: ReactNode) => (
+	<Page disableFooterMargin background='/images/pages/shuttle-ai/hero.png'>
+		{children}
+	</Page>
+)
