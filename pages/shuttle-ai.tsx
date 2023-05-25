923ec16068d2d8dca2e39b2359a1515b4cbee1a0
Senofy <hello@digitalera.dev>
2023-05-24 20:19:13 -0400

    Added behind the curtain steps

--- pages/shuttle-ai.tsx
+++ pages/shuttle-ai.tsx
@@ -6,20 +6,16 @@ import Image from 'next/image'
 
 export default function ShuttleAIPage() {
 	return (
-		<div className='group relative flex-grow overflow-hidden'>
+		<div
+			className='group relative flex-grow overflow-hidden bg-center bg-repeat'
+			style={{ backgroundImage: "url('/images/pages/shuttle-ai/stars.png')" }}
+		>
 			<Image
 				src='/images/pages/shuttle-ai/noise-group.png'
 				alt=''
 				width={1440}
 				height={1023}
 				className='absolute -top-[5.5rem] w-full mix-blend-overlay'
-			/>
-			<Image
-				src='/images/pages/shuttle-ai/stars.png'
-				alt=''
-				width={1427}
-				height={321}
-				className='absolute -top-[5.5rem] w-full'
 			/>
 			<Hero />
 			<Info />
