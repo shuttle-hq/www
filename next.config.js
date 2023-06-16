5899568095ed7d86190c6d027d6a1f2f39214dfc
Guga Gongadze <guga@shuttle.rs>
2023-06-16 19:07:19 +0400

    Fix: Only allow iframe requests from the same origin (#80)
    * fix: only allow iframe requests from the same origin
    
    * fix: remove test file

--- next.config.js
+++ next.config.js
@@ -1,31 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	i18n: {
-		locales: ['en'],
-		defaultLocale: 'en',
-	},
-	images: {
-		domains: ['github.com'],
-	},
-	async rewrites() {
-		return [
-			{
-				source: '/rss.xml',
-				destination: '/api/rss',
-			},
-		]
-	},
-	redirects() {
-		return [
-			{
-				source: '/blog',
-				destination: '/blog/tags/all',
-				permanent: true,
-			},
-		]
-	},
-	reactStrictMode: true,
-	swcMinify: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  images: {
+    domains: ['github.com'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/rss.xml',
+        destination: '/api/rss',
+      },
+    ]
+  },
+  redirects() {
+    return [
+      {
+        source: '/blog',
+        destination: '/blog/tags/all',
+        permanent: true,
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+        ],
+      },
+    ]
+  },
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
