61971ae6a4a7ed7e5a7f5baef12402f476dc6975
Guga Gongadze <gugongadze@gmail.com>
2023-06-16 17:08:00 +0400

    feat: launchpad issue 1: getting started

--- pages/launchpad/issues/index.tsx
+++ pages/launchpad/issues/index.tsx
@@ -1,72 +1,60 @@
 import LaunchpadIssues from 'components/sections/Launchpad/LaunchpadIssues'
 import { SITE_URL } from 'lib/constants'
-import { getAllTags, getSortedIssues } from 'lib/launchpad/issues'
-import { GetStaticPropsContext, GetStaticPropsResult } from 'next'
+import { getSortedIssues } from 'lib/launchpad/issues'
+import { GetStaticPropsResult } from 'next'
 import { NextSeo } from 'next-seo'
 import { useRouter } from 'next/router'
-import { ParsedUrlQuery } from 'querystring'
 import React from 'react'
 
-interface Params extends ParsedUrlQuery {
-	readonly tag: string
-}
+export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
+  const issues = getSortedIssues(0)
 
-export async function getStaticProps({
-	params,
-}: GetStaticPropsContext<Params>): Promise<GetStaticPropsResult<Props>> {
-	const tag = params?.tag !== undefined && params.tag !== 'all' ? params.tag : ''
-	const issues = getSortedIssues(0, tag ? [tag] : undefined)
-
-	const tags = getAllTags()
-
-	return {
-		props: {
-			issues,
-			tags,
-		},
-	}
+  return {
+    props: {
+      issues,
+    },
+  }
 }
 
 interface Props {
-	readonly issues: ReturnType<typeof getSortedIssues>
-	readonly tags: string[]
+  readonly issues: ReturnType<typeof getSortedIssues>
 }
 
 function Issues(props: Props) {
-	const { issues, tags } = props
+  const { issues } = props
 
-	const router = useRouter()
+  const router = useRouter()
 
-	const meta_title = 'Shuttle Launchpad'
-	const meta_description = 'Shuttle Launchpad'
+  const meta_title = 'Shuttle Launchpad'
+  const meta_description = 'Shuttle Launchpad'
 
-	return (
-		<>
-			<NextSeo
-				title={meta_title}
-				description={meta_description}
-				openGraph={{
-					title: meta_title,
-					description: meta_description,
-					url: SITE_URL + router.pathname,
-					images: [
-						{
-							url: `${SITE_URL}images/og/og-image.jpg`,
-						},
-					],
-				}}
-				additionalLinkTags={[
-					{
-						rel: 'alternate',
-						type: 'application/rss+xml',
-						href: `${SITE_URL}rss.xml`,
-					},
-				]}
-			/>
+  return (
+    <>
+      <NextSeo
+        title={meta_title}
+        description={meta_description}
+        openGraph={{
+          title: meta_title,
+          description: meta_description,
+          url: SITE_URL + router.pathname,
+          images: [
+            {
+              url: `${SITE_URL}images/og/og-image.jpg`,
+            },
+          ],
+        }}
+        additionalLinkTags={[
+          {
+            rel: 'alternate',
+            type: 'application/rss+xml',
+            href: `${SITE_URL}rss.xml`,
+          },
+        ]}
+      />
 
-			<LaunchpadIssues tags={tags} issues={issues} />
-		</>
-	)
+      <LaunchpadIssues issues={issues} />
+    </>
+  )
 }
 
 export default Issues
