edb33239dc3288eead2f2a1380ff82dabec243b6
Guga Gongadze <gugongadze@gmail.com>
2023-09-05 12:36:48 +0400

    fix: update blog snippets

--- pages/launchpad/issues/[slug].tsx
+++ pages/launchpad/issues/[slug].tsx
@@ -20,6 +20,7 @@ import clsx from 'clsx'
 import { CopyButton } from 'components/elements'
 import { TwitterTweetEmbed } from 'react-twitter-embed'
 import { Issue, getAllIssueSlugs, getIssueData } from 'lib/launchpad/issues'
+import { Pre } from 'components/blog/Pre'
 
 export async function getStaticPaths() {
 	const paths = getAllIssueSlugs()
@@ -82,69 +83,6 @@ export async function getStaticProps({
 			} as Issue,
 		},
 	}
-}
-
-const Pre = ({ children, ...props }: any) => {
-	let line = 0
-
-	function getChildren(children: any): string[] {
-		return [children].flat().flatMap((child: any) => {
-			if (typeof child === 'string') {
-				return child
-			}
-
-			return getChildren(child.props.children)
-		})
-	}
-
-	const code = getChildren(children.props.children).join('')
-
-	return (
-		<div className={clsx('relative')}>
-			<CopyButton code={code} className='absolute right-2 top-2 inline-flex items-center' />
-			<pre
-				{...props}
-				className={clsx(
-					'!border !border-black/10 !bg-white !pr-16 !text-sm text-[#525151] dark:!border-white/10 dark:!bg-black dark:text-[#BEBEBE] [&>*]:!bg-white dark:[&>*]:!bg-black',
-					props.className ?? 'language-'
-				)}
-			>
-				{{
-					...children,
-					props: {
-						...children.props,
-						className: children.props.className ?? 'language-',
-						children: [
-							...[children.props.children].flat().flatMap((child) => {
-								if (typeof child === 'string') {
-									const [head, ...tail] = child.split('\n')
-									return [
-										head,
-										...tail.flatMap((child) => {
-											line++
-
-											return [
-												'\n',
-												// <span
-												// 	key={head}
-												// 	className='mr-4 inline-block w-4 select-none text-right italic text-[rgb(92,99,112)] last:hidden'
-												// >
-												// 	{line}
-												// </span>,
-												child,
-											]
-										}),
-									]
-								} else {
-									return child
-								}
-							}),
-						],
-					},
-				}}
-			</pre>
-		</div>
-	)
 }
 
 const mdxComponents: MDXRemoteProps['components'] = {
