d44d66b647936f86706e8091c2769517b42d7b60
Guga Gongadze <guga@shuttle.rs>
2023-09-05 10:54:31 +0400

    fix: sitemas (#114)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-	siteUrl: process.env.SITE_URL || 'https://example.com',
+	siteUrl: process.env.SITE_URL || 'https://www.shuttle.rs',
 	generateRobotsTxt: true,
 }
