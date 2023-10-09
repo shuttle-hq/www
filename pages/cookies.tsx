d3343366198123d731b9891cc4471df3a0dbcbdc
Guga Gongadze <gugongadze@gmail.com>
2023-10-09 18:29:26 +0400

    feat: cookies

---
+++ pages/cookies.tsx
@@ -0,0 +1,179 @@
+export default function Cookies() {
+	return (
+		<main className='mx-auto mt-24 w-full max-w-7xl px-5 sm:px-10 lg:mt-28'>
+			<div className='mb-[98px]'>
+				<h1 className='mb-[19px] text-[64px] font-bold text-[#D8D8D8]'>Cookies Policy</h1>
+				<p className='mb-6 text-xl'>
+					This Cookies Policy forms part of our general Privacy Policy
+					<a href='https://shuttle.rs/privacy' target='_blank'>
+						https://www.shuttle.rs/privacy
+					</a>
+					.
+				</p>
+				<p className='mb-6 text-xl'>
+					This Cookies Policy will give you more information as to the technologies we use on{' '}
+					<a href='https://www.shuttle.rs/' target='_blank'>
+						https://www.shuttle.rs
+					</a>{' '}
+					(our Website) and your rights to restrict such use.
+				</p>
+			</div>
+
+			<div className='mb-10 mt-10'>
+				<h2 className='mb-5 text-[32px] font-bold text-[#D8D8D8]'>1. How we use cookies</h2>
+				<p className='mb-5 text-xl'>
+					Our Website uses cookies to distinguish you from other users of our Website. This helps us to
+					provide you with a good experience when you browse the Website and allows us to improve both
+					our Website and your experience.
+				</p>
+				<p className='mb-5 text-xl'>
+					By ticking the box of our popup Cookies Policy you are agreeing to our use of cookies as
+					defined herein. If you do not consent to the use of cookies, you may block or disable them
+					through your browser settings as explained in this Cookies Policy. However, blocking or
+					disabling cookies may have a negative impact on your use of this Website.
+				</p>
+
+				<h2 className='mb-5 text-[32px] font-bold text-[#D8D8D8]'>2. Cookies</h2>
+				<p className='mb-5 text-xl'>
+					A cookie is a small file of letters and numbers that we store on your browser or the hard drive
+					of your computer if you agree, which can be used to identify that you have previously visited a
+					website, or to store small bits of information locally with you.
+				</p>
+				<p className='mb-5 text-xl'>
+					There are two different types of cookies: session and persistent cookies. Session cookies
+					expire at the end of a browser session, so right after you exit the browser, while persistent
+					cookies are stored on your computer between browsing sessions until they expire or you decide
+					to delete them.
+				</p>
+			</div>
+			<div className='mb-10 mt-10 text-xl'>
+				<h2 className='mb-5 text-[32px] font-bold text-[#D8D8D8]'>3. The cookies we use</h2>
+
+				<p className='mb-5 text-xl'>We use the following types of cookies:</p>
+
+				<table className='mb-20 table-fixed text-xs lg:text-sm'>
+					<tr className='text-left children:border children:border-gray-900 children:px-1 children:py-1 lg:children:px-4 lg:children:py-3'>
+						<th className='text-[#C2C2C2]'>Strictly necessary cookies</th>
+						<td>
+							We use these cookies for functionality of the Website’s platforms. These cookies are
+							essential in order to enable users to move around the Website and use its features,
+							including in order to log in to our services.
+						</td>
+					</tr>
+					<tr className='text-left children:border children:border-gray-900 children:px-1 children:py-1 lg:children:px-4 lg:children:py-3'>
+						<th className='text-[#C2C2C2]'>Analytical or performance cookies</th>
+						<td>
+							These allow us to recognise and count the number of visitors and to see how visitors
+							move around our Website when they are using it. This helps us improve the way our
+							Website works, for example, by ensuring that users are finding what they are looking
+							for easily.
+						</td>
+					</tr>
+					<tr className='text-left children:border children:border-gray-900 children:px-1 children:py-1 lg:children:px-4 lg:children:py-3'>
+						<th className='text-[#C2C2C2]'>Functionality cookies.</th>
+						<td>
+							These are used to remember the session and keep the user logged in if they have checked
+							“Remember me” when logging in.
+						</td>
+					</tr>
+					<tr className='text-left children:border children:border-gray-900 children:px-1 children:py-1 lg:children:px-4 lg:children:py-3'>
+						<th className='text-[#C2C2C2]'>Targeting or advertising cookies. </th>
+						<td>
+							These cookies record your visit to our website, the pages you have visited and the
+							links you have followed. We will use this information to make our website and the
+							advertising displayed on it more relevant to your interests.
+						</td>
+					</tr>
+				</table>
+			</div>
+
+			<div className='mb-10 mt-10 text-xl'>
+				<h2 className='mb-5 text-[32px] font-bold text-[#D8D8D8]'>
+					4. We use the following third party cookies
+				</h2>
+
+				<table className='mb-20 table-fixed text-xs lg:text-sm'>
+					<tr className='text-left children:border children:border-gray-900 children:px-1 children:py-1 lg:children:px-4 lg:children:py-3'>
+						<th className='text-[#C2C2C2]'>Cookie </th>
+						<th className='text-[#C2C2C2]'>Name</th>
+						<th className='text-[#C2C2C2]'>Purpose</th>
+					</tr>
+					<tr className='text-left children:border children:border-gray-900 children:px-1 children:py-1 lg:children:px-4 lg:children:py-3'>
+						<td>Functionality Cookies</td>
+						<td>Auth0</td>
+						<td>
+							We use Okta’s Auth0 to help us authenticate users. More information on Auth0’s
+							processing of personal data can be found here:
+							<a href='https://www.okta.com/privacy-policy/' target='_blank'>
+								https://www.okta.com/privacy-policy/
+							</a>
+						</td>
+					</tr>
+				</table>
+			</div>
+
+			<div className='mb-10 mt-10 text-xl'>
+				<h2 className='mb-5 text-[32px] font-bold text-[#D8D8D8]'>5. How can I manage my cookies?</h2>
+				<p className='mb-5 text-xl'>
+					Please note that third parties (including, for example, advertising networks and providers of
+					external services like web traffic analysis services) may also use cookies, over which we have
+					no control. These cookies are likely to be analytical/performance cookies or targeting cookies.
+				</p>
+
+				<p className='mb-5 text-xl'>
+					You may refuse to accept cookies or delete those already through the setting on your web
+					browser. You may find more information here:
+				</p>
+
+				<ol className='mb-5 list-disc space-y-4 pl-[25px] text-xl'>
+					<li className='mb-2'>
+						<a href='https://support.apple.com/en-gb/HT201265' target='_blank'>
+							Safari
+						</a>
+					</li>
+					<li className='mb-2'>
+						<a
+							href='https://support.mozilla.org/en-US/kb/cookies-information-websites-store-on-your-computer'
+							target='_blank'
+						>
+							Firefox
+						</a>
+					</li>
+					<li className='mb-2'>
+						<a
+							href='https://support.microsoft.com/en-us/help/17442/windows-internet-explorer-delete-manage-cookies#ie=ie-11'
+							target='_blank'
+						>
+							Internet explorer
+						</a>
+					</li>
+					<li className='mb-2'>
+						<a href='https://support.google.com/chrome/answer/95647?hl=en' target='_blank'>
+							Chrome
+						</a>
+					</li>
+					<li className='mb-2'>
+						<a
+							href='https://support.microsoft.com/en-us/windows/microsoft-edge-browsing-data-and-privacy-bb8174ba-9d73-dcf2-9b4a-c582b4e640dd'
+							target='_blank'
+						>
+							Microsoft Edge
+						</a>
+					</li>
+				</ol>
+
+				<p className='mb-5 text-xl'>
+					To find information relating to other browsers, visit the browser developer’s website.
+				</p>
+				<p className='mb-5 text-xl'>
+					However, if you use your browser settings to block all cookies you may not be able to access
+					all or parts of our Website.
+				</p>
+
+				<h3 className='mb-2 text-2xl font-bold text-[#D8D8D8]'>6. Contact us</h3>
+
+				<p className='mb-5 text-xl'>You may contact us by email at: hello@shuttle.rs</p>
+			</div>
+		</main>
+	)
+}
