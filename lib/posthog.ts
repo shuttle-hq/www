0b178b031d0c2dca60b6764c39a3b22e537ff141
Guga Gongadze <gugongadze@gmail.com>
2023-10-25 16:33:29 +0400

    feat: githu star

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -1,11 +1,6 @@
 import posthog from 'posthog-js'
 
-export type TrackingEvent =
-	| 'pro-plan-intent'
-	| 'community-plan-intent'
-	| 'team-plan-intent'
-	| 'api-key-copied'
-	| 'contact-intent'
+export type TrackingEvent = 'github_star_cta'
 
 export type TrackingProperties = {
 	domain: 'console' | 'www'
