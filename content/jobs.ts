5b748b9ff9b9505ed1ef108ae68787b4177d734f
Joshua Mo <joshua.mo.876@gmail.com>
2024-01-05 14:07:49 +0000

    feat: add cto job

--- content/jobs.ts
+++ content/jobs.ts
@@ -5,6 +5,10 @@ export const Jobs: PositionProps[] = [
 		name: 'Staff Cloud Engineer',
 		link: 'https://www.ycombinator.com/companies/shuttle/jobs/LpJTT9u-staff-cloud-engineer',
 	},
+	{
+		name: 'Chief Technical Officer (CTO)',
+		link: 'https://www.ycombinator.com/companies/shuttle/jobs/7LOjSaN-chief-technical-officer-cto',
+	},
 ]
 
 export default Jobs
