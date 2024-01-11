5c30ebad08c517ff02e3750e1dfd7f076ef70148
Guga Gongadze <gugongadze@gmail.com>
2024-01-11 15:35:58 +0400

    fix: remove fallback

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ import { PostHogProvider } from 'posthog-js/react'
 if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
 	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
 		api_host: '/ingest',
-		ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.posthog.com',
+		ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
 		// Enable debug mode in development
 		loaded: (posthog) => {
 			if (process.env.NODE_ENV === 'development') posthog.debug()
