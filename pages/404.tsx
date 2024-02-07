3e2a53164475162251df77be3aceca37fb7fefe7
Hamir Mahal <hamirmahal@gmail.com>
2024-02-07 13:38:14 -0800

    fix: formatting inconsistencies in `pages/` dir

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
 	return (
 		<>
-			<div className='m-auto w-10/12 max-w-2xl overflow-visible pt-32 pb-32 leading-none'>
+			<div className='m-auto w-10/12 max-w-2xl overflow-visible pb-32 pt-32 leading-none'>
 				<div className='m-auto w-full max-w-2xl'>
 					<div className='m-auto pb-4 text-6xl'>
 						<span className='dark:text-brand-600 m-auto font-bold'>Oops!</span>
