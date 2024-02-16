62e65eba7964c04020b2c39de890370a94b886fd
Hamir Mahal <hamirmahal@gmail.com>
2024-02-16 15:58:54 -0800

    refactor: remove deprecated `prettier` property

--- prettier.config.js
+++ prettier.config.js
@@ -5,7 +5,6 @@ module.exports = {
 	embeddedLanguageFormatting: 'auto',
 	htmlWhitespaceSensitivity: 'css',
 	insertPragma: false,
-	jsxBracketSameLine: false,
 	jsxSingleQuote: true,
 	printWidth: 115,
 	proseWrap: 'preserve',
