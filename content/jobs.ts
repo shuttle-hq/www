cbf6e5b452f4534b8529351dbad0275918967fdb
Guga Gongadze <guga@shuttle.rs>
2024-04-16 16:34:42 +0400

    fix: remove old jobs (#303)

--- content/jobs.ts
+++ content/jobs.ts
@@ -1,14 +1,5 @@
 import { PositionProps } from 'components/sections/JobOpenings'
 
-export const Jobs: PositionProps[] = [
-	{
-		name: 'Head of Engineering',
-		link: 'https://www.ycombinator.com/companies/shuttle/jobs/7LOjSaN-head-of-engineering',
-	},
-	{
-		name: 'Head of Product',
-		link: 'https://www.ycombinator.com/companies/shuttle/jobs/dBqnJAi-head-of-product',
-	},
-]
+export const Jobs: PositionProps[] = []
 
 export default Jobs
