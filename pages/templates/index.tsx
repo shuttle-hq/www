1ff2f7290305fb18c48805fba96fd22db9f97c41
Guga Gongadze <gugongadze@gmail.com>
2024-04-17 12:40:17 +0400

    fix: movile

--- pages/templates/index.tsx
+++ pages/templates/index.tsx
@@ -4,6 +4,8 @@ import toml from '@iarna/toml'
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import TemplateCard from 'components/sections/Templates/TemplateCard'
 import TemplateFilterGroup from 'components/sections/Templates/TemplateFilterGroup'
+import FilterDesktop from 'components/sections/Templates/FilterDesktop'
+import FilterMobile from 'components/sections/Templates/FilterMobile'
 
 export const TEMPLATES_URL = 'https://raw.githubusercontent.com/shuttle-hq/shuttle-examples/main/templates.toml'
 
@@ -105,55 +107,30 @@ export default function Templates({ tags, useCases, starters }: InferGetStaticPr
 				<p className='text-2xl'>Optional description of something.</p>
 			</div>
 
-			<section className='mt-32 grid grid-cols-1 justify-between gap-[30px] px-[128px] md:grid-cols-4'>
-				<div className='col-span-1'>
-					<div className='mb-7 flex w-full justify-between'>
-						<span className='text-md'>Filter</span>
+			<section className='mt-32 grid grid-cols-1 justify-between gap-[30px] px-0 md:grid-cols-4 md:px-[128px]'>
+				<FilterDesktop
+					search={search}
+					setSearch={setSearch}
+					setSelectedTags={setSelectedTags}
+					setSelectedUseCases={setSelectedUseCases}
+					tags={tags}
+					useCases={useCases}
+					selectedUseCases={selectedUseCases}
+					selectedTags={selectedTags}
+				/>
 
-						<button
-							className='rounded-[23px] bg-[#FFFFFF1A] px-3 py-[2px] text-[#7A7A7A]'
-							onClick={() => {
-								setSelectedTags([])
-								setSelectedUseCases([])
-								setSearch('')
-							}}
-						>
-							Clear
-						</button>
-					</div>
+				<FilterMobile
+					search={search}
+					setSearch={setSearch}
+					setSelectedTags={setSelectedTags}
+					setSelectedUseCases={setSelectedUseCases}
+					tags={tags}
+					useCases={useCases}
+					selectedUseCases={selectedUseCases}
+					selectedTags={selectedTags}
+				/>
 
-					<input
-						type='text'
-						placeholder='Search'
-						className='mb-5 w-full rounded-[14px] border border-[#FFFFFF1A] bg-[#FFFFFF0D] px-5 py-3 text-[#C2C2C266]'
-						value={search}
-						onChange={(e) => setSearch(e.target.value)}
-					/>
-
-					<div className='mb-6 rounded-[14px] bg-[linear-gradient(84.16deg,_#F4292933_-7.25%,_#FFD76F0B_35.45%,_#38D4E933_97%)] p-[1px]'>
-						<button className='w-full rounded-[14px] border border-[#FFFFFF1A] bg-black px-6 py-3 text-center'>
-							Contribute
-						</button>
-					</div>
-
-					<hr className='mb-6 border-[#FFFFFF1A]' />
-
-					<TemplateFilterGroup
-						items={useCases}
-						selectedItems={selectedUseCases}
-						setSelectedItems={setSelectedUseCases}
-						title='Use Case'
-					/>
-
-					<TemplateFilterGroup
-						items={tags}
-						selectedItems={selectedTags}
-						setSelectedItems={setSelectedTags}
-						title='Tags'
-					/>
-				</div>
-
-				<div className='col-span-3 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
+				<div className='col-span-3 grid grid-cols-1 gap-6 px-[28px] md:grid-cols-2 md:px-0 lg:grid-cols-3'>
 					{filteredStarters.slice(0, 9).map((starter) => (
 						<TemplateCard key={starter.key} starter={starter} />
 					))}
