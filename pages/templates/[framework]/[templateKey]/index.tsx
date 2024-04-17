ebb95e823c7509a9587b1bbc2112d4a4297c104e
Guga Gongadze <gugongadze@gmail.com>
2024-04-17 11:33:17 +0400

    fix: templates details

--- pages/templates/[framework]/[templateKey]/index.tsx
+++ pages/templates/[framework]/[templateKey]/index.tsx
@@ -7,6 +7,7 @@ import { Markdown } from 'components/elements/Markdown'
 import Link from 'next/link'
 import Image from 'next/image'
 import { CopyButton } from 'components/elements'
+import { Logo } from 'components/svgs'
 
 export const getStaticPaths = (async () => {
 	const response = await fetch(TEMPLATES_URL)
@@ -92,6 +93,7 @@ export const getStaticProps = (async (context) => {
 }>
 
 export default function TemplateDetails({ starter }: InferGetStaticPropsType<typeof getStaticProps>) {
+	console.log('starter:', starter)
 	return (
 		<section className='mx-auto my-[65px] w-full max-w-screen-xl px-32'>
 			<Link className='mb-14 flex items-center gap-1' href='/templates'>
@@ -111,19 +113,34 @@ export default function TemplateDetails({ starter }: InferGetStaticPropsType<typ
 						/>
 
 						<div className='mb-3 flex w-full items-center justify-between rounded-xl bg-[#121212] text-xs text-head'>
-							<pre className='overflow-x-scroll p-4'>{`cargo shuttle init`}</pre>
-							<CopyButton code={`cargo shuttle init`} width={20} height={20} />
+							<pre className='overflow-x-scroll p-4'>{`cargo shuttle init --from ${starter.path}`}</pre>
+							<CopyButton
+								code={`cargo shuttle init --from ${starter.path}`}
+								width={20}
+								height={20}
+							/>
 						</div>
 
-						<button className='mb-5 flex w-full justify-center gap-1 rounded-lg bg-gradient px-6 py-2'>
-							<Link href={`/templates/${starter.path}`}>
-								<span className='text-sm text-black'>Launch</span>{' '}
-							</Link>
-							<Image src='/images/templates/arrow.svg' width={12} height={12} alt='arrow' />
-						</button>
+						<div className='mb-5 w-full rounded-xl bg-[linear-gradient(85.52deg,_#FC540C_-7.27%,_rgba(255,215,111,0.72)_108.87%)] p-[1px]'>
+							<button className='flex w-full items-baseline justify-center gap-1 rounded-xl bg-[#0F0F0F] px-6 py-2'>
+								<Link href={`/templates/${starter.path}`}>
+									<span className='text-sm text-white'>Launch</span>{' '}
+								</Link>
+								<Image src='/images/templates/arrow.svg' width={10} height={10} alt='arrow' />
+							</button>
+						</div>
 
 						<div className='mb-5 flex w-full justify-between'>
-							<p className='text-[#7A7A7A]'>Use cases</p>
+							<p className='text-[#7A7A7A]'>Type</p>
+
+							<div className='flex gap-2'>
+								<p>Starter</p>
+							</div>
+						</div>
+						<hr className='mb-5 w-full border-[#434343] border-[0.5]' />
+
+						<div className='mb-5 flex w-full justify-between'>
+							<p className='text-[#7A7A7A]'>Use case</p>
 
 							<div>
 								<p>
@@ -136,12 +153,22 @@ export default function TemplateDetails({ starter }: InferGetStaticPropsType<typ
 						<hr className='mb-5 w-full border-[#434343] border-[0.5]' />
 
 						<div className='mb-5 flex w-full justify-between'>
+							<p className='text-[#7A7A7A]'>Author</p>
+
+							<div className='flex gap-2'>
+								<Image src='/images/logo.png' width={24} height={24} alt='Shuttle logo' />
+								<p>Shuttle</p>
+							</div>
+						</div>
+						<hr className='mb-5 w-full border-[#434343] border-[0.5]' />
+
+						<div className='mb-5 flex w-full justify-between'>
 							<span className='text-[#7A7A7A]'>Tags</span>
 							<div>
 								{starter.tags.map((tag) => (
 									<span
 										key={tag}
-										className='rounded-xl border border-orange px-3 py-1 text-sm text-white'
+										className='rounded-xl border border-[#434343] px-3 py-1 text-sm text-white'
 									>
 										{tag}
 									</span>
@@ -150,20 +177,35 @@ export default function TemplateDetails({ starter }: InferGetStaticPropsType<typ
 						</div>
 					</div>
 
-					<button className='flex w-full justify-center gap-1 rounded-[14px] border border-[0.5] border-orange px-6 py-2'>
-						<Link
-							href={`https://github.com/shuttle-hq/shuttle-examples/blob/main/${starter.path}`}
-							target='_blank'
-						>
-							<span className='text-sm text-white'>Github Repo</span>
-						</Link>
-					</button>
+					<div className='mb-3 w-full rounded-xl bg-[linear-gradient(46.52deg,_rgba(244,41,41,0.2)_-19.65%,_rgba(255,138,63,0.2)_27.61%,_rgba(229,233,56,0.2)_101.96%)] p-[1px]'>
+						<button className='flex w-full items-baseline justify-center gap-1 rounded-xl bg-[#0F0F0F] px-6 py-2'>
+							<Link
+								href={`https://github.com/shuttle-hq/shuttle-examples/blob/main/${starter.path}`}
+								target='_blank'
+							>
+								<span className='text-sm text-white'>Article</span>
+							</Link>
+						</button>
+					</div>
+
+					<div className='mb-5 w-full rounded-xl bg-[linear-gradient(46.52deg,_rgba(244,41,41,0.2)_-19.65%,_rgba(255,138,63,0.2)_27.61%,_rgba(229,233,56,0.2)_101.96%)] p-[1px]'>
+						<button className='flex w-full items-baseline justify-center gap-1 rounded-xl bg-[#0F0F0F] px-6 py-2'>
+							<Link
+								href={`https://github.com/shuttle-hq/shuttle-examples/blob/main/${starter.path}`}
+								target='_blank'
+							>
+								<span className='text-sm text-white'>Github Repo</span>
+							</Link>
+						</button>
+					</div>
 				</div>
 
 				<div className='col-span-2 rounded-3xl border border-[#434343] p-8'>
 					<h1 className='mb-5 text-[36px] font-bold leading-10 text-[#D8D8D8]'>{starter.title}</h1>
 
+					<p className='mb-8'>{'//README'}</p>
 					<Markdown body={starter.readme} />
+					<p>{'//README'}</p>
 				</div>
 			</div>
 		</section>
