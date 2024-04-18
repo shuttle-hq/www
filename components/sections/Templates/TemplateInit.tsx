b621bd87d25d1bcd7cc8ed094d309894ca5673c9
Guga Gongadze <gugongadze@gmail.com>
2024-04-18 12:22:21 +0400

    fix: scrollbar color

--- components/sections/Templates/TemplateInit.tsx
+++ components/sections/Templates/TemplateInit.tsx
@@ -3,7 +3,13 @@ import { CopyButton } from 'components/elements'
 export default function TemplateInit({ path }: { path: string }) {
 	return (
 		<div className='mb-3 flex w-full items-center justify-between rounded-xl bg-[#121212] text-xs text-head'>
-			<pre className='overflow-x-scroll p-4'>{`cargo shuttle init --from ${path}`}</pre>
+			<pre
+				style={{
+					scrollbarColor: 'black transparent',
+					scrollbarWidth: 'thin',
+				}}
+				className='overflow-x-scroll p-4'
+			>{`cargo shuttle init --from ${path}`}</pre>
 			<CopyButton code={`cargo shuttle init --from ${path}`} width={16} height={16} />
 		</div>
 	)
