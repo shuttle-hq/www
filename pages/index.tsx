11d690e790d1986b5826a2a9cd1ace48e464691f
Guga Gongadze <gugongadze@gmail.com>
2024-04-23 14:36:13 +0400

    feat: gsap

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,13 +16,13 @@ import { GetStaticPropsResult } from 'next'
 import { StarterAttrs } from '../components/elements/Starter'
 import { QuestionAttrs } from '../components/sections/FrequentlyAskedQuestions'
 import { landingQuestions, FeaturedStartersContent } from '../content'
-import { useScroll, motion, useMotionValueEvent, useSpring } from 'framer-motion'
+import { useScroll, motion, useMotionValueEvent, useSpring, useAnimation, useAnimate } from 'framer-motion'
 import GreyRocket from 'components/sections/Home/GreyRocket'
 import RocketColor1 from 'components/sections/Home/RocketColor1'
 import RocketColor3 from 'components/sections/Home/RocketColor3'
 import RocketColor2 from 'components/sections/Home/RocketColor2'
 import RocketColorFull from 'components/sections/Home/RocketColorFull'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import Step1 from 'components/sections/Home/Step1'
 import Step2 from 'components/sections/Home/Step2'
 import Step3 from 'components/sections/Home/Step3'
@@ -35,6 +35,12 @@ import Step3Right from 'components/sections/Home/Step3Right'
 import Step3Left from 'components/sections/Home/Step3Left'
 import Step3Left2 from 'components/sections/Home/Step3Left2'
 import Step4 from 'components/sections/Home/Step4'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+import RocketColor4 from 'components/sections/Home/RocketColorFull'
+
+gsap.registerPlugin(useGSAP, ScrollTrigger)
 
 export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
 	const posts = getSortedPosts(3)
@@ -69,74 +75,48 @@ interface Props {
 }
 
 export default function Home({ posts, starters, questions, stargazersCount }: Props) {
-	// const { scrollYProgress, scrollY } = useScroll()
-	// const scaleX = useSpring(scrollYProgress)
-	// console.log('scaleX:', scaleX)
-
-	// useMotionValueEvent(scrollY, 'change', (latest) => {
-	// 	console.log('Page scroll: ', latest)
-	// })
 	const [activeStep, setActiveStep] = useState(0)
+	const container = useRef<HTMLDivElement | null>(null)
+	const tl = useRef<any>()
 
-	useEffect(() => {
-		function handleScroll(event: any) {
-			if (event.keyCode === 37) {
-				if (activeStep > 0) {
-					setActiveStep((a) => a - 1)
-				}
-			} else if (event.keyCode === 39) {
-				if (activeStep < 3) {
-					setActiveStep((a) => a + 1)
-				}
-			}
-		}
+	useGSAP(
+		() => {
+			const rockets = gsap.utils.toArray('.rocket')
 
-		addEventListener('keyup', handleScroll, true)
-
-		return () => {
-			removeEventListener('keyup', handleScroll, true)
-		}
-	}, [activeStep])
+			rockets.forEach((rocket: any, index) => {
+				gsap.fromTo(
+					rocket,
+					{ autoAlpha: 0, y: 100 }, // from state
+					{
+						autoAlpha: 1,
+						y: 0, // to state
+						scrollTrigger: {
+							trigger: container.current,
+							start: 'top top+=100',
+							end: '+=100', // Adjust end value based on how long you want the pin effect
+							scrub: true,
+							markers: true, // Remove in production, used for debugging
+							pin: true, // Pin the container when in view
+							pinSpacing: false,
+							onEnter: () => console.log('Animation start for', rocket),
+							onLeaveBack: () => console.log('Reversing animation for', rocket),
+						},
+					}
+				)
+			})
+		},
+		{ scope: container }
+	) // <-- scope for selector text (optional)
 
 	return (
-		<main className='pointer-events-none overflow-hidden text-body'>
-			<div className='mt-[226px] w-full text-center'>
-				<h1 className='text-[72px] text-[#F0F0F0]'>Launch System</h1>
-				<p>Blast off in t-minus 3, 2, 1...</p>
-			</div>
-			<div className='mx-auto my-20 flex h-screen max-w-7xl justify-center overflow-y-hidden'>
-				<div
-					className={`${
-						activeStep === 0 ? 'opacity-100' : 'opacity-0'
-					} absolute flex h-full w-full justify-center transition-all duration-700 ease-in`}
-				>
-					<RocketColor1 className='absolute' />
-					<Step1 className='absolute' />
-					<Step1Left />
-					<Step1Bottom />
-					<Step1Right />
-					<Vectors1 />
-				</div>
-				<div
-					className={`${
-						activeStep === 1 ? 'opacity-100' : 'opacity-0'
-					} absolute flex w-full justify-center transition-all duration-700 ease-in`}
-				>
-					<RocketColor2 className='absolute' />
-					<Step2 className='absolute' />
-				</div>
-				<div
-					className={`${
-						activeStep === 2 ? 'opacity-100' : 'opacity-0'
-					} absolute flex h-full w-full justify-center transition-all duration-700 ease-in`}
-				>
-					<RocketColor3 className='absolute' />
-					<Step3 className='absolute' />
-					<Step3Vectors />
-					<Step3Left />
-					<Step3Left2 />
-					<Step3Right />
-				</div>
+		<main className='text-body'>
+			{/* <Hero /> */}
+
+			<div className='rockets relative h-[3000px]' ref={container}>
+				<RocketColor1 className='rocket-1 rocket absolute left-0 top-0' />
+				<RocketColor2 className='rocket-2 rocket absolute left-0 top-0' />
+				<RocketColor3 className='rocket-3 rocket absolute left-0 top-0' />
+				<RocketColor4 className='rocket-4 rocket absolute left-0 top-0' />
 			</div>
 		</main>
 	)
