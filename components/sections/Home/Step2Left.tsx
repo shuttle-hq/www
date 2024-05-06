3d316609b843b967892bf09e5a08800bb2c780db
Guga Gongadze <gugongadze@gmail.com>
2024-05-06 16:26:32 +0400

    fix: step 2 right

--- components/sections/Home/Step2Left.tsx
+++ components/sections/Home/Step2Left.tsx
@@ -4,7 +4,7 @@ export default function Step2Left({ className, id }: { className?: string; id?: 
 	return (
 		<div
 			id={id}
-			className='z-10 w-full rounded-[24px] border border-[#525150] bg-black p-4 sm:w-[500px] lg:w-[300px] 2xl:w-[500px] 2xl:p-7'
+			className='z-10 w-full rounded-3xl border border-[#525150] p-4 backdrop-blur-sm sm:w-[500px] lg:w-[300px] 2xl:w-[500px] 2xl:p-7'
 		>
 			<div className='mb-3 flex items-baseline gap-[6px] text-[#F0F0F0]'>
 				<h1 className='text-xl leading-none 2xl:text-2xl'>Infrastructure from Code</h1>
