bdd38906607037db9bf4e853a86c25ef571f7d2f
Guga Gongadze <guga@shuttle.rs>
2024-05-14 17:52:38 +0400

    feat: seo metadata (#325)

--- pages/ai.tsx
+++ pages/ai.tsx
@@ -3,28 +3,35 @@ import { Page } from 'components/templates'
 import { ReactNode } from 'react'
 import Image from 'next/image'
 import { Waitlist } from 'components/sections/ShuttleAI/Waitlist'
+import { NextSeo } from 'next-seo'
 
 export default function ShuttleAIPage() {
 	return (
-		<div
-			className='group relative flex-grow overflow-hidden bg-center bg-repeat'
-			style={{ backgroundImage: "url('/images/pages/shuttle-ai/stars.png')" }}
-		>
-			<Image
-				src='/images/pages/shuttle-ai/noise-group.png'
-				alt=''
-				width={1440}
-				height={1023}
-				className='absolute -top-[5.5rem] w-full mix-blend-overlay'
+		<>
+			<NextSeo
+				title='Shuttle AI - Deploy Apps from Prompts | Cloud Development Tools'
+				description='Build and deploy cloud applications effortlessly using Shuttle AI. Discover the power of single-prompt app deployment in our Rust-centric product.'
 			/>
-			<Hero />
-			<Info />
-			<Steps />
-			<Waitlist
-				cta='The fastest way to build and deploy in the cloud.'
-				description='Even faster than deploying a "hello world". Sign up for the waitlist!'
-			/>
-		</div>
+			<div
+				className='group relative flex-grow overflow-hidden bg-center bg-repeat'
+				style={{ backgroundImage: "url('/images/pages/shuttle-ai/stars.png')" }}
+			>
+				<Image
+					src='/images/pages/shuttle-ai/noise-group.png'
+					alt=''
+					width={1440}
+					height={1023}
+					className='absolute -top-[5.5rem] w-full mix-blend-overlay'
+				/>
+				<Hero />
+				<Info />
+				<Steps />
+				<Waitlist
+					cta='The fastest way to build and deploy in the cloud.'
+					description='Even faster than deploying a "hello world". Sign up for the waitlist!'
+				/>
+			</div>
+		</>
 	)
 }
 
