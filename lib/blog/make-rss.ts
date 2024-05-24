93419ccae3ed417265e10142064ce50b2fdb522b
Joshua Mo <joshua.mo.876@gmail.com>
2024-05-24 17:56:53 +0100

    feat: rss feed

--- lib/blog/make-rss.ts
+++ lib/blog/make-rss.ts
@@ -2,15 +2,21 @@ import { APP_NAME, SITE_URL } from '../constants'
 import { getSortedPosts, Post } from './posts'
 import fs from 'fs'
 
-const postXML = (post: Post): string => `
-<item>
+const postXML = (post: Post): string => {
+	let sanitizedTitle = post.title
+	let sanitizedDescription = post.description
+
+	sanitizedTitle.replaceAll('&', 'and')
+	sanitizedDescription.replaceAll('&', 'and')
+	return ` <item>
   <guid>${SITE_URL}blog/${post.url}</guid>
-  <title>${post.title}</title>
+  <title>${sanitizedTitle}</title>
   <link>${SITE_URL}blog/${post.url}</link>
-  <description>${post.description}</description>
+  <description>${sanitizedDescription}</description>
   <pubDate>${new Date(post.date).toUTCString()}</pubDate>
 </item>
 `
+}
 
 const rssXML = (posts: readonly Post[]): string => `
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
@@ -20,7 +26,7 @@ const rssXML = (posts: readonly Post[]): string => `
     <description>Latest news from ${APP_NAME}</description>
     <language>en</language>
     <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
-    <atom:link href="${SITE_URL}/rss.xml" rel="self" type="application/rss+xml"/>
+    <atom:link href="${SITE_URL}rss.xml" rel="self" type="application/rss+xml"/>
     ${posts.map(postXML).join('')}
   </channel>
 </rss>
@@ -28,11 +34,11 @@ const rssXML = (posts: readonly Post[]): string => `
 
 const posts = getSortedPosts()
 
-if (!window) {
-	fs.writeFile('./public/rss.xml', rssXML(posts), (err) => {
-		if (err != null) {
-			console.error(err)
-			process.exit(1)
-		}
-	})
-}
+export const exportedPosts = rssXML(posts).replaceAll('&', '&amp;')
+
+fs.writeFile('./public/rss.xml', exportedPosts, (err) => {
+	if (err != null) {
+		console.error(err)
+		process.exit(1)
+	}
+})
