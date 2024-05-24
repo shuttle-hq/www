ed6c2649d987366a52b1db88e0b52569acfd01de
Joshua Mo <102877324+joshua-mo-143@users.noreply.github.com>
2024-05-24 18:22:00 +0100

    feat: add RSS feed content header (#337)

--- pages/api/rss.ts
+++ pages/api/rss.ts
@@ -2,5 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { exportedPosts } from 'lib/blog/make-rss'
 
 export default function rss_feed(_: NextApiRequest, res: NextApiResponse<string>) {
+	res.setHeader('Content-Type', 'text/xml')
 	res.status(200).send(exportedPosts)
 }
