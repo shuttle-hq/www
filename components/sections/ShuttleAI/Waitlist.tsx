9af7ec362fa4835373929f37d3c7752a47226808
Senofy <hello@digitalera.dev>
2024-06-21 01:16:18 -0500

    Removed waitlist prop types

--- components/sections/ShuttleAI/Waitlist.tsx
+++ components/sections/ShuttleAI/Waitlist.tsx
@@ -2,11 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { FC } from 'react'
 
-interface WaitListProps {
-	cta: string
-	description: string
-	subDescription?: string
-}
+interface WaitListProps {}
 
 export const Waitlist: FC<WaitListProps> = () => {
 	return (
