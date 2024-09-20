6da06f3684761ab459da4baf14968c8b30958a30
Senofy <63175905+Senofy@users.noreply.github.com>
2024-09-20 03:16:01 -0500

    Reverted launch systems to have desktop positioning

--- components/sections/Home/LaunchSystem/LaunchSystemStep1.tsx
+++ components/sections/Home/LaunchSystem/LaunchSystemStep1.tsx
@@ -7,7 +7,7 @@ import RocketColor1 from '../RocketColor1'
 
 export default function LaunchSystemStep1() {
 	return (
-		<div className='relative mb-[200px] flex flex-col items-center gap-8 bg-black '>
+		<div className='relative mb-[200px] flex flex-col items-center gap-8 bg-black 2xl:justify-center'>
 			<Xarrow
 				start='top-1'
 				end='left-1'
@@ -55,9 +55,9 @@ export default function LaunchSystemStep1() {
 				path='grid'
 				divContainerProps={{ className: 'hidden 2xl:block' }}
 			/>
-			<div className='flex h-auto w-full flex-col items-center justify-between gap-8 '>
+			<div className='flex h-auto w-full flex-col items-center justify-between gap-8 2xl:h-[90vh]'>
 				<Step1Top id='top-1' />
-				<div className='flex w-full flex-col items-center justify-between gap-8 '>
+				<div className='flex w-full flex-col items-center justify-between gap-8 2xl:flex-row'>
 					<Step1Left id='left-1' />
 					<Step1Right id='right-1' />
 				</div>
--- components/sections/Home/LaunchSystem/LaunchSystemStep2.tsx
+++ components/sections/Home/LaunchSystem/LaunchSystemStep2.tsx
@@ -7,7 +7,7 @@ import Step2Bottom from './Step2Bottom'
 
 export default function LaunchSystemStep2() {
 	return (
-		<div className='relative mb-[200px] flex flex-col items-center gap-8 bg-black '>
+		<div className='relative mb-[200px] flex flex-col items-center gap-8 bg-black 2xl:justify-center'>
 			<Xarrow
 				start='top-2'
 				end='left-2'
@@ -55,9 +55,9 @@ export default function LaunchSystemStep2() {
 				path='grid'
 				divContainerProps={{ className: 'hidden 2xl:block' }}
 			/>
-			<div className='flex h-auto w-full flex-col items-center justify-between gap-8 '>
+			<div className='flex h-auto w-full flex-col items-center justify-between gap-8 2xl:h-[90vh]'>
 				<Step2Top id='top-2' />
-				<div className='flex w-full flex-col items-center justify-between gap-8'>
+				<div className='flex w-full flex-col items-center justify-between gap-8 2xl:flex-row'>
 					<Step2Left id='left-2' />
 					<Step2Right id='right-2' />
 				</div>
