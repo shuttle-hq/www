0578fd805b9ffaf2c3e9453b50aadf387760d074
Joshua Mo <102877324+joshua-mo-143@users.noreply.github.com>
2024-09-27 19:25:35 +0100

    fix: revert pull request #356 (#359)
    Fixes an accidental merge that broke production.

--- components/elements/CodeBlock.tsx
+++ components/elements/CodeBlock.tsx
@@ -24,41 +24,23 @@ function cargo(Prism: any) {
 SyntaxHighlighter.registerLanguage('cargo', cargo)
 
 interface Props {
-	readonly id?: string
 	readonly language: string
 	readonly code: string
 	readonly showLineNumbers?: boolean
-	readonly biggestAmountOfLines?: number
 	readonly className?: string
 	readonly copyBtn?: boolean
-	readonly style?: React.CSSProperties
 }
 
-export default function CodeBlock({
-	id,
-	code,
-	language,
-	showLineNumbers,
-	biggestAmountOfLines,
-	className,
-	copyBtn,
-	style,
-}: Props) {
+export default function CodeBlock({ code, language, showLineNumbers, className, copyBtn }: Props) {
 	useWindowSize()
 
-	console.log(code)
-
 	return (
-		<div
-			className={clsx('relative -mt-4 rounded !bg-black p-4 pt-0 shadow-lg', className)}
-			style={style}
-			id={id}
-		>
-			{copyBtn && <CopyButton code={code} className='sticky right-2 top-2 inline-flex items-center' />}
+		<div className={clsx('relative rounded !bg-black p-4 shadow-lg', className)}>
+			{copyBtn && <CopyButton code={code} className='absolute right-2 top-2 inline-flex items-center' />}
 
 			<HeightMagic>
 				<SyntaxHighlighter
-					className='!m-0 overflow-hidden !p-0 text-sm dark:!bg-black dark:[&>*]:!bg-black'
+					className='!m-0 overflow-auto !p-0 text-sm dark:!bg-black dark:[&>*]:!bg-black'
 					language={language}
 					style={oneDark}
 					showLineNumbers={showLineNumbers}
@@ -69,11 +51,7 @@ export default function CodeBlock({
 						background: 'black',
 					}}
 				>
-					{code +
-						Array.from({ length: biggestAmountOfLines - code.split('\n').length })
-							.fill(0)
-							.map((_, i) => '\n')
-							.join('')}
+					{code}
 				</SyntaxHighlighter>
 			</HeightMagic>
 		</div>
