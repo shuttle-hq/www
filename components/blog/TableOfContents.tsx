e89330d4681d915dba0e8c65f2d226e868751448
Joshua Mo <joshua.mo.876@gmail.com>
2024-11-15 11:33:01 +0000

    refactor: fix styling

--- components/blog/TableOfContents.tsx
+++ components/blog/TableOfContents.tsx
@@ -6,23 +6,14 @@ interface HeaderProps {
 	lvl: number
 }
 
-function get_margin(lvl: number): number {
-	if (lvl >= 1 && lvl <= 6) {
-		return lvl * 4
-	} else {
-		throw new Error(`Margin level is invalid: ${lvl}`)
-	}
-}
-
 const TOCHeader = ({ slug, content, lvl }: HeaderProps) => {
-	const margin = get_margin(lvl)
+	const margin = `${lvl - 1}rem`
 	return (
 		<li
-			className={`pl-${margin} text-xl text-slate-300 transition duration-150 ease-in-out hover:translate-x-2 hover:text-white`}
+			className={`block text-xl text-slate-300 transition duration-150 ease-in-out hover:translate-x-2 hover:text-white`}
+			style={{ marginLeft: margin }}
 		>
-			<a href={`#${slug}`} className={`pl-${margin}`}>
-				- {content}
-			</a>
+			<a href={`#${slug}`}>- {content}</a>
 		</li>
 	)
 }
@@ -35,6 +26,7 @@ export const TableOfContents = ({ toc }: Props) => {
 	return (
 		<div className='mb-12 mt-16 border-b-2 border-t-2 border-b-[#A2A2A2] border-t-[#A2A2A2] pb-8 pt-4'>
 			<p className='pb-4 pt-4 text-2xl font-bold leading-10 lg:text-3xl'>Table of Contents</p>
+
 			<ol>
 				{toc.map((header) => (
 					<TOCHeader key={header.slug} slug={header.slug} content={header.content} lvl={header.lvl} />
