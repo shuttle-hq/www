5c08f986f73d5eb03776ed5550918e526fda611c
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2025-02-20 14:38:33 +0100

    2025

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -160,7 +160,7 @@ const Footer: FC<FooterProps> = ({ disableMargin }) => {
         </div>
         <div className="grid grid-cols-2 divide-x divide-black/10 dark:divide-white/10 lg:col-span-2">
           <div className="lg:grid lg:grid-cols-2 lg:divide-x lg:divide-black/10 dark:lg:divide-white/10">
-            <p className="px-8 pb-5 lg:px-16 lg:pb-9">© 2024 Shuttle</p>
+            <p className="px-8 pb-5 lg:px-16 lg:pb-9">© 2025 Shuttle</p>
             <div className="hidden lg:block" />
           </div>
           <div className="lg:grid lg:grid-cols-2 lg:divide-x lg:divide-black/10 dark:lg:divide-white/10">
