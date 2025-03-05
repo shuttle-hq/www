4547adbe21e82c3b1a7fe285916b36830055a7d5
anton <antonabramov1990@gmail.com>
2025-03-05 14:44:03 +0100

    [FRO-9] refactoring pricing grids

--- components/sections/Pricing/UsageBasedPricing/PricingCard.tsx
+++ components/sections/Pricing/UsageBasedPricing/PricingCard.tsx
@@ -4,23 +4,33 @@ import * as React from "react";
 import { PriceDisplay } from "./PriceDisplay";
 
 interface PricingCardProps {
+  iconSrc: string;
   title: string;
   price: string;
   unit: string;
 }
 
 export const PricingCard: React.FC<PricingCardProps> = ({
+  iconSrc,
   title,
   price,
   unit,
 }) => {
   return (
-    <article className="flex flex-col flex-1 shrink justify-center self-stretch p-8 my-auto bg-black border border-solid basis-0 border-slate-600 border-opacity-30 min-w-60 rounded-[32px] max-md:px-5">
-      <h2 className="text-base font-bold text-center text-neutral-200">
-        {title}
-      </h2>
-      <div className="self-center mt-2">
-        <PriceDisplay price={price} unit={unit} />
+    <article className="w-full md:w-1/2 px-2 my-2" role="listitem">
+      <div className="flex grow shrink gap-4 items-center self-stretch px-6 py-8 my-auto bg-black border border-solid border-white border-opacity-30 rounded-[32px] max-md:px-5">
+        <img
+          src={iconSrc}
+          alt={`${title} icon`}
+          className="object-contain shrink-0 self-stretch my-auto w-10 aspect-square"
+        />
+        <div className="flex-1 shrink self-stretch my-auto basis-0">
+          <h3 className="text-base font-bold text-neutral-200">{title}</h3>
+          <div className="flex gap-2 items-center">
+            <p className="text-2xl text-right text-white">{price}</p>
+            <p className="text-sm text-center text-neutral-400">{unit}</p>
+          </div>
+        </div>
       </div>
     </article>
   );
--- components/sections/Pricing/UsageBasedPricing/PricingGrid.tsx
+++ components/sections/Pricing/UsageBasedPricing/PricingGrid.tsx
@@ -3,9 +3,40 @@
 import * as React from "react";
 import { PricingCard } from "./PricingCard";
 
-export const PricingGrid: React.FC = () => {
+const pricingData = [
+  {
+    iconSrc:
+      "https://cdn.builder.io/api/v1/image/assets/c8cd7228d5b149c59753a0e9d2f2b879/784fda3e6f130bc2e22891ea1b21e0202aa08bb8d94cd7f8940ad980315ff5b0?placeholderIfAbsent=true",
+    title: "Database storage",
+    price: "$0.12",
+    unit: "/ GB / month",
+  },
+  {
+    iconSrc:
+      "https://cdn.builder.io/api/v1/image/assets/c8cd7228d5b149c59753a0e9d2f2b879/aa53cb4fd7cef1f025cbea539951ed51fbf5316fcb6dd258dfbdf99295830c5d?placeholderIfAbsent=true",
+    title: "Build minutes",
+    price: "$0.025",
+    unit: "/ min",
+  },
+  {
+    iconSrc:
+      "https://cdn.builder.io/api/v1/image/assets/c8cd7228d5b149c59753a0e9d2f2b879/742cdd175f363a3b1cf139e692262a767cea7c001af51cb17ab7b00d09447049?placeholderIfAbsent=true",
+    title: "Network egress",
+    price: "$0.10",
+    unit: "/ GB",
+  },
+  {
+    iconSrc:
+      "https://cdn.builder.io/api/v1/image/assets/c8cd7228d5b149c59753a0e9d2f2b879/2ffdafe375215a3f6eea0bd894e46102b6265aee60dca28127e31ed04200ad97?placeholderIfAbsent=true",
+    title: "Compute",
+    price: "$0.06",
+    unit: "/ vCPU hour",
+  },
+];
+
+export default function Cards() {
   return (
-    <div className="mx-auto max-w-7xl">
+    <div className="mx-auto max-w-7xl w-full">
       <div className="flex flex-row flex-wrap justify-center items-center gap-8 xl:gap-0 xl:flex-nowrap mx-[-0.5rem]">
         <div className="flex flex-col gap-5 px-10 min-w-0 text-center xl:text-left xl:w-1/2">
           <h1 className="text-[40px] md:text-5xl font-bold text-gray-300">
@@ -16,23 +47,26 @@ export const PricingGrid: React.FC = () => {
             you use, when you need it.
           </div>
         </div>
-        <section className="rounded-[32px] px-2 xl:w-1/2">
-          <div className="flex flex-wrap gap-4 items-center w-full max-md:max-w-full">
-            <PricingCard
-              title="Database storage:"
-              price="$0.12"
-              unit="/ GB / month"
-            />
-            <PricingCard title="Build minutes:" price="$0.025" unit="/ min" />
+        <section
+          className="lg:w-1/2 xl:px-12 w-full"
+          aria-label="Pricing Cards"
+        >
+          <div
+            className="flex flex-col md:flex-row flex-wrap items-center w-full max-md:max-w-full"
+            role="list"
+          >
+            <PricingCard {...pricingData[0]} />
+            <PricingCard {...pricingData[1]} />
           </div>
-          <div className="flex flex-wrap gap-4 items-center mt-4 w-full max-md:max-w-full">
-            <PricingCard title="Network egress:" price="$0.10" unit="/ GB" />
-            <PricingCard title="Compute:" price="$0.06" unit="/ vCPU hour" />
+          <div
+            className="flex flex-col md:flex-row flex-wrap items-center w-full max-md:max-w-full"
+            role="list"
+          >
+            <PricingCard {...pricingData[2]} />
+            <PricingCard {...pricingData[3]} />
           </div>
         </section>
       </div>
     </div>
   );
-};
-
-export default PricingGrid;
+}
