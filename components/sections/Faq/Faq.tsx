3ab01521b22c31b2181a0f8dfa61d1d05b02c254
anton <antonabramov1990@gmail.com>
2025-03-06 17:43:43 +0200

    [FRO-9] Fix build issues.

--- components/sections/Faq/Faq.tsx
+++ components/sections/Faq/Faq.tsx
@@ -1,20 +1,20 @@
 "use client";
 
 import * as React from "react";
+import { FaqItem as FaqItemType } from "../Pricing/faqData";
 import { FaqItem } from "./FaqItem";
 
 interface FaqItemProps {
-  question: string;
-  answer: string | string[];
+  faqData: FaqItemType[];
 }
 
-export const Faq: React.FC = ({ faqData }: any) => {
+export const Faq: React.FC<FaqItemProps> = ({ faqData }) => {
   return (
     <section className="max-w-[1024px] my-16 mx-auto">
       <h1 className="text-left md:text-center font-gradual mb-8 md:mb-16 text-[40px] md:text-5xl font-bold text-black dark:text-head lg:text-4">
         Frequently Asked Questions
       </h1>
-      {faqData.map((faq: FaqItemProps, index: number) => (
+      {faqData.map((faq, index) => (
         <FaqItem key={index} question={faq.question} answer={faq.answer} />
       ))}
     </section>
