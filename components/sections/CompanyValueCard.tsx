10821efadc6ceeb3ed71394fe0c1f49bfc6102a7
anton <antonabramov1990@gmail.com>
2025-03-27 13:55:42 +0100

    fix fonts

--- components/sections/CompanyValueCard.tsx
+++ components/sections/CompanyValueCard.tsx
@@ -1,7 +1,7 @@
 interface ValueCardProps {
-  emoji: string
-  title: string
-  subtitle: string
+  emoji: string;
+  title: string;
+  subtitle: string;
 }
 
 export default function CompanyValueCard({
@@ -13,13 +13,13 @@ export default function CompanyValueCard({
     <article className="h-full w-full" role="listitem">
       <div className="flex flex-col h-full gap-1 p-8 bg-black border border-solid border-white border-opacity-30 rounded-[32px]">
         <div className="text-2xl">{emoji}</div>
-        <h4 className="font-atkinson font-normal text-xl leading-8 tracking-1 text-grey100">
+        <h4 className="font-normal text-xl leading-6 tracking-1 text-grey100">
           {title}
         </h4>
-        <p className="font-atkinson font-normal text-base leading-6 tracking-1 text-body">
+        <p className="font-normal text-base leading-6 tracking-1 text-body">
           {subtitle}
         </p>
       </div>
     </article>
-  )
+  );
 }
