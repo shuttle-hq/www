ad1078b595b03b9a99919e53e00c41d11d1db909
insmac <maciej.bodek@gmail.com>
2025-09-05 18:03:54 +0200

    Migrate from `@mapbox/rehype-prism` to `rehype-prism-plus`

--- pages/blog/[year]/[month]/[day]/[slug].tsx
+++ pages/blog/[year]/[month]/[day]/[slug].tsx
@@ -16,7 +16,7 @@ import slug from "rehype-slug";
 
 // @ts-ignore
 import toc from "markdown-toc";
-import rehypePrism from "@mapbox/rehype-prism";
+import rehypePrism from "rehype-prism-plus";
 import { DISCORD_URL, SITE_URL } from "lib/constants";
 import { GetStaticPropsContext, GetStaticPropsResult } from "next";
 import { ParsedUrlQuery } from "querystring";
