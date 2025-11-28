c0717569588f4ff525dce881e15e607a74f0aafc
dcodes <101001810+dcodesdev@users.noreply.github.com>
2025-11-28 16:35:01 +0300

    Blog Sidebar Scrollable (#569)

--- components/sections/BlogSidebar.tsx
+++ components/sections/BlogSidebar.tsx
@@ -16,7 +16,7 @@ interface BlogSidebarProps {
 const BlogSidebar: FC<BlogSidebarProps> = ({ toc }) => {
   return (
     <div className="relative w-full">
-      <div className="w-full space-y-6 lg:sticky lg:top-24">
+      <div className="w-full space-y-6 lg:sticky lg:top-24 lg:max-h-[calc(100vh-6rem)] lg:overflow-y-auto lg:pr-2">
         <div className="rounded-3xl border border-black/10 px-6 py-4 dark:border-white/10">
           <div className="mb-2 dark:text-head">On this page</div>
           <SidebarTOC toc={toc} />
